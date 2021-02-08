0e30a27245e009060490e03b16dfc03e17a5cda5
guoandong <guoandong@jinyuyun.top>
2021-02-08 17:06:26 +0800

    fix:添加实数数据爬取重试机制

--- src/main/java/com/gad/epidemicmanage/task/RealTimeDataTask.java
+++ src/main/java/com/gad/epidemicmanage/task/RealTimeDataTask.java
@@ -29,52 +29,60 @@ public class RealTimeDataTask implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        String html = HttpUnitUtil.getHtmlPage(GlobalConstant.REAL_TIME_URL);
-        if ("".equals(html)) {
-            log.error("国内实时数据获取页面失败");
-            return;
-        }else {
-            log.info("国内实时数据获取页面成功，开始分析");
-        }
-        try{
+        int i = 0;
+        int maxTimes = 5;
 
-            Document document = Jsoup.parse(html);
+        while(i < maxTimes){
+            String html = HttpUnitUtil.getHtmlPage(GlobalConstant.REAL_TIME_URL);
+            if ("".equals(html)) {
+                log.error("国内实时数据获取页面失败");
+                return;
+            }else {
+                log.info("国内实时数据获取页面成功，开始第 "+ i+1 +" 次分析");
+            }
 
-            RealTimeData realTimeData = new RealTimeData();
-            //地区
-            Element e1 = document.getElementsByClass("Virus_1-1-295_3U87rg").get(0);
-            realTimeData.setPlace(e1.text());
-            //现存确诊
-            Element e2 = document.getElementsByClass("VirusSummarySix_1-1-295_TxrYcT").get(0);
-            realTimeData.setExitDiagnosis(Integer.parseInt(e2.text()));
-            //累计确诊
-            Element e3 = document.getElementsByClass("VirusSummarySix_1-1-295_TxrYcT").get(4);
-            realTimeData.setCountDiagnosis(Integer.parseInt(e3.text()));
-            //境外输入
-            Element e4 = document.getElementsByClass("VirusSummarySix_1-1-295_TxrYcT").get(5);
-            realTimeData.setAbroad(Integer.parseInt(e4.text()));
-            //无症状
-            Element e5 = document.getElementsByClass("VirusSummarySix_1-1-295_TxrYcT").get(1);
-            realTimeData.setAsymptomatic(Integer.parseInt(e5.text()));
-            //现存疑似
-            Element e6 = document.getElementsByClass("VirusSummarySix_1-1-295_TxrYcT").get(2);
-            realTimeData.setExitSuspected(Integer.parseInt(e6.text()));
-            //现存重症
-            Element e7 = document.getElementsByClass("VirusSummarySix_1-1-295_TxrYcT").get(3);
-            realTimeData.setExitSevere(Integer.parseInt(e7.text()));
-            //累计治愈
-            Element e8 = document.getElementsByClass("VirusSummarySix_1-1-295_TxrYcT").get(6);
-            realTimeData.setCountCure(Integer.parseInt(e8.text()));
-            //累计死亡
-            Element e9 = document.getElementsByClass("VirusSummarySix_1-1-295_TxrYcT").get(7);
-            realTimeData.setCountDeath(Integer.parseInt(e9.text()));
-            //当天日期
-            realTimeData.setDate(CommonUtil.todayDate());
+            try{
+                Document document = Jsoup.parse(html);
 
-            realTimeDataService.addRealTimeData(realTimeData);
-            log.info("国内实时数据获取完毕");
-        }catch (Exception e){
-            log.error("国内获取实时疫情数据异常:"+e);
+                RealTimeData realTimeData = new RealTimeData();
+                //地区
+                Element e1 = document.getElementsByClass("Virus_1-1-295_3U87rg").get(0);
+                realTimeData.setPlace(e1.text());
+                //现存确诊
+                Element e2 = document.getElementsByClass("VirusSummarySix_1-1-295_TxrYcT").get(0);
+                realTimeData.setExitDiagnosis(Integer.parseInt(e2.text()));
+                //累计确诊
+                Element e3 = document.getElementsByClass("VirusSummarySix_1-1-295_TxrYcT").get(4);
+                realTimeData.setCountDiagnosis(Integer.parseInt(e3.text()));
+                //境外输入
+                Element e4 = document.getElementsByClass("VirusSummarySix_1-1-295_TxrYcT").get(5);
+                realTimeData.setAbroad(Integer.parseInt(e4.text()));
+                //无症状
+                Element e5 = document.getElementsByClass("VirusSummarySix_1-1-295_TxrYcT").get(1);
+                realTimeData.setAsymptomatic(Integer.parseInt(e5.text()));
+                //现存疑似
+                Element e6 = document.getElementsByClass("VirusSummarySix_1-1-295_TxrYcT").get(2);
+                realTimeData.setExitSuspected(Integer.parseInt(e6.text()));
+                //现存重症
+                Element e7 = document.getElementsByClass("VirusSummarySix_1-1-295_TxrYcT").get(3);
+                realTimeData.setExitSevere(Integer.parseInt(e7.text()));
+                //累计治愈
+                Element e8 = document.getElementsByClass("VirusSummarySix_1-1-295_TxrYcT").get(6);
+                realTimeData.setCountCure(Integer.parseInt(e8.text()));
+                //累计死亡
+                Element e9 = document.getElementsByClass("VirusSummarySix_1-1-295_TxrYcT").get(7);
+                realTimeData.setCountDeath(Integer.parseInt(e9.text()));
+                //当天日期
+                realTimeData.setDate(CommonUtil.todayDate());
+                log.info("页面数据分析完毕，存入数据库");
+                realTimeDataService.addRealTimeData(realTimeData);
+                log.info("国内实时数据已保存");
+                break;
+            }catch (Exception e){
+                log.error("第 "+ i+1 +" 次国内分析实时疫情数据异常:"+e);
+                i++;
+            }
+
         }
     }
 }
